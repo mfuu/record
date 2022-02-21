e05352cb06759c1626933d4a1b1f0cb931bc0068
mf-note <446310957@qq.com>
2022-02-21 09:50:21 +0800

    面试算法题

--- code/RangeList.js
+++ code/RangeList.js
@@ -86,4 +86,34 @@ class RangeList {
   }
 }
 
-export default RangeList
+const rl = new RangeList() // or `const rl = new RangeList([1, 5])`
+
+rl.add([1, 5])
+rl.print()
+
+rl.add([10, 20])
+rl.print()
+
+rl.add([20, 20])
+rl.print()
+
+rl.add([20, 21])
+rl.print()
+
+rl.add([2, 4])
+rl.print()
+
+rl.add([3, 8])
+rl.print()
+
+rl.remove([10, 10])
+rl.print()
+
+rl.remove([10, 11])
+rl.print()
+
+rl.remove([15, 17])
+rl.print()
+
+rl.remove([3, 19])
+rl.print()
